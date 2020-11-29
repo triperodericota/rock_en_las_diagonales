bcc1f4a6c4f69a4a07bff1d01845c13fdf82cb9c
Nicolas Mc Cormack <nicolasmccormack22@gmail.com>
2020-11-29 13:42:28 -0300

    improve search feature

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,11 +11,24 @@ class WelcomeController < ApplicationController
 
   # GET /search
   def search
-    search_string = "%#{params[:model].html_safe}%"
-    @artists = Artist.with_search_name(search_string)
-    @events = Event.fields_with_input_string(search_string)
-    @products = Product.where.has {(title =~ search_string)}
-    @any_result = true unless @artists.empty? & @events.empty? & @products.empty?
+    if params[:model].present?
+      search_string = "%#{params[:model].html_safe}%"
+      @artists = Artist.with_search_name(search_string)
+      @events = Event.fields_with_input_string(search_string)
+      @products = Product.with_search_title(search_string)
+      @result = (@artists.to_a << @events.to_a << @products.to_a).flatten.collect do |result_elem|
+        class_name = result_elem.class.to_s.downcase
+        {'id': class_name + result_elem.id.to_s, name: result_elem.to_s, type: result_elem.class.to_s,
+        url: result_elem.base_uri }
+      end
+    end
+    if request.xml_http_request?
+      respond_to do |format|
+        format.json {
+          render json: {result: @result}
+        }
+      end
+    end
   end
 
   def show_cities
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
-7/* eslint no-console:0 */
+
+/* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
@@ -10,6 +11,7 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
+
 
 import "bootstrap"
 import "../src/application.scss"
@@ -30,130 +32,12 @@ document.addEventListener("turbolinks:load", () => {
 console.log('Hello World from Webpacker');
 
 window.Mercadopago.setPublishableKey("TEST-6ccb9c47-6884-4fcb-967c-83d3906ac14f");
-window.Mercadopago.getIdentificationTypes();
-
-document.onload = function() {
 
 
-    document.getElementById("cardNumber").addEventListener('change', guessPaymentMethod);
-
-    function guessPaymentMethod(event) {
-        let cardnumber = document.getElementById("cardNumber").value;
-        if (cardnumber.length >= 6) {
-            let bin = cardnumber.substring(0, 6);
-            window.Mercadopago.getPaymentMethod({
-                "bin": bin
-            }, setPaymentMethod);
-        }
-    };
-
-    function setPaymentMethod(status, response) {
-        if (status == 200) {
-            let paymentMethod = response[0];
-            document.getElementById('paymentMethodId').value = paymentMethod.id;
-
-            if (paymentMethod.additional_info_needed.includes("issuer_id")) {
-                getIssuers(paymentMethod.id);
-            } else {
-                getInstallments(
-                    paymentMethod.id,
-                    document.getElementById('transactionAmount').value
-                );
-            }
-        } else {
-            alert(`payment method info error: ${response}`);
-        }
-    }
-
-
-    function getIssuers(paymentMethodId) {
-        window.Mercadopago.getIssuers(
-            paymentMethodId,
-            setIssuers
-        );
-    }
-
-    function setIssuers(status, response) {
-        if (status == 200) {
-            let issuerSelect = document.getElementById('issuer');
-            response.forEach(issuer => {
-                let opt = document.createElement('option');
-            opt.text = issuer.name;
-            opt.value = issuer.id;
-            issuerSelect.appendChild(opt);
-        })
-            ;
-
-            getInstallments(
-                document.getElementById('paymentMethodId').value,
-                document.getElementById('transactionAmount').value,
-                issuerSelect.value
-            );
-        } else {
-            alert(`issuers method info error: ${response}`);
-        }
-    }
-
-
-    function getInstallments(paymentMethodId, transactionAmount, issuerId) {
-        window.Mercadopago.getInstallments({
-            "payment_method_id": paymentMethodId,
-            "amount": parseFloat(transactionAmount),
-            "issuer_id": issuerId ? parseInt(issuerId) : undefined
-        }, setInstallments);
-    }
-
-    function setInstallments(status, response) {
-        if (status == 200) {
-            document.getElementById('installments').options.length = 0;
-            response[0].payer_costs.forEach(payerCost => {
-                let opt = document.createElement('option');
-            opt.text = payerCost.recommended_message;
-            opt.value = payerCost.installments;
-            document.getElementById('installments').appendChild(opt);
-        })
-            ;
-        } else {
-            alert(`installments method info error: ${response}`);
-        }
-    }
-
-
-    var doSubmit = false;
-    document.getElementById('paymentForm').addEventListener('submit', getCardToken);
-
-    function getCardToken(event) {
-        event.preventDefault();
-        if (!doSubmit) {
-            let $form = document.getElementById('paymentForm');
-            window.Mercadopago.createToken($form, setCardTokenAndPay);
-            return false;
-        }
-    };
-
-    function setCardTokenAndPay(status, response) {
-        if (status == 200 || status == 201) {
-            let form = document.getElementById('paymentForm');
-            let card = document.createElement('input');
-            card.setAttribute('name', 'token');
-            card.setAttribute('type', 'hidden');
-            card.setAttribute('value', response.id);
-            form.appendChild(card);
-            doSubmit = true;
-            form.submit();
-        } else {
-            alert("Verify filled data!\n" + JSON.stringify(response, null, 4));
-        }
-    };
-
-}
-
-// PRODUCTS JS
-
-function displayPhoto(event) {
-    var output = document.getElementById('imagePreview');
-    output.src = URL.createObjectURL(event.target.files[0]);
-}
+$(document).on("change", "#photo", function (){
+    var output = $("#imagePreview");
+    output.attr("src",URL.createObjectURL(event.target.files[0]));
+})
 
 function takePhotoNumber(event) {
     var id = event.target.id;
@@ -161,26 +45,28 @@ function takePhotoNumber(event) {
     return photo_number;
 }
 
-function openDialog(event) {
-    photo_number = takePhotoNumber(event)
-    document.getElementById('photo-'.concat(photo_number.toString())).click();
-}
+$(document).on("change", "input[id|='photo']", function (event) {
+    alert("catch event");
+    var photo_number = takePhotoNumber(event);
+    var tag_id_string = "#photo-preview-".concat(photo_number.toString());
+    console.log($(tag_id_string));
+    $(tag_id_string).attr("src",URL.createObjectURL(event.target.files[0]));
+})
 
-function displayProductPhoto(event) {
-    photo_number = takePhotoNumber(event);
-    var output = document.getElementById('photo-preview-'.concat(photo_number.toString()));
-    output.src = URL.createObjectURL(event.target.files[0]);
-}
+$(document).on("click", "img[id|='photo-preview']", function (event) {
+    var photo_number = takePhotoNumber(event);
+    $("#photo-".concat(photo_number.toString())).click();
+})
 
-$(document).on("click","#submitOrder",function() {
-    if(document.getElementById("terms").checked){
+$(document).on("click", "#submitOrder", function () {
+    if (document.getElementById("terms").checked) {
         $("#new_order").submit();
-    }else{
+    } else {
         alert("Debes aceptar los términos antes de continuar")
     }
 });
 
-$(document).on("click", "#withDelivery", function(){
+$(document).on("click", "#withDelivery", function () {
     $("#order_delivery").val(true)
     $("#state").prop("disabled", false)
     $("#order_buyer_address_street_name").prop("disabled", false)
@@ -213,23 +99,24 @@ $(document).on("change", "#state", function () {
     });
 });
 
-$(document).on("keyup","#model-search",function(){
+$(document).on("keyup", "#model-search", function () {
     var search_val = $(this).val();
     $.ajax({
-        url:"/search",
+        url: "/search",
         method: "GET",
         dataType: "json",
         data: {model: search_val},
-        success: function(response) {
+        success: function (response) {
             var response = response["result"];
             console.log(response);
             var response_len = response.length;
             $("#search-result").empty();
-            for(var i=0; i < response_len; i++){
-                console.log("<li value='"+response[i]['id']+"'"+"><a href='"+response[i]['url']+"'"+">"+response[i]['name']+"</a></li>");
-                $("#search-result").append("<li value='"+response[i]['id']+"'"+"><a href='"+response[i]['url']+"'"+"><h5>"+response[i]['name']+"</h5></a><small>"+response[i]['type']+"</small></li>");
+            for (var i = 0; i < response_len; i++) {
+                console.log("<li value='" + response[i]['id'] + "'" + "><a href='" + response[i]['url'] + "'" + ">" + response[i]['name'] + "</a></li>");
+                $("#search-result").append("<li value='" + response[i]['id'] + "'" + "><a href='" + response[i]['url'] + "'" + "><h5>" + response[i]['name'] + "</h5></a><small>" + response[i]['type'] + "</small></li>");
 
             }
         }
     });
 });
+
